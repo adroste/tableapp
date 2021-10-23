c6d939d80ed2891f5121f6efb83db56b8a9be122
Alexander Droste <mail@adroste.com>
2021-10-23 16:10:09 +0200

    chore(client): update dep jimp to v0.16.1, integrate api changes

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,19 +8,9 @@ import Jimp from 'jimp';
  * @returns {Promise<string>} Resolves to base64 thumbnail
  */
 export async function createThumbnailFromBase64Image(base64Image) {
-    async function imageToBase64(img) {
-        return new Promise((resolve, reject) => {
-            img.getBase64(Jimp.MIME_PNG, (err, data) => {
-                if (err)
-                    reject(err);
-                resolve(data);
-            });
-        });
-    }
-
     const img = await Jimp.read(Buffer.from(base64Image.replace(/^data:image\/png;base64,/, ""), 'base64'));
     img.cover(80, 80);
-    return await imageToBase64(img);
+    return await img.getBase64Async(Jimp.MIME_PNG);
 }
 
 
