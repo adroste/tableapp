b464337abd38476bcb9585f5df8fbf66cf904682
Alexander Droste <mail@adroste.com>
2021-10-25 09:16:17 +0200

    fix(client): customConfig did not support single and double quotes in content

--- client/generate-react-custom-config.sh
+++ client/generate-react-custom-config.sh
@@ -5,6 +5,6 @@ beginswith() { case $2 in "$1"*) true ;; *) false ;; esac }
 printf 'window.customConfig = {};\n'
 env | while IFS='=' read -r name value; do
     if beginswith TABLE_ "$name"; then
-        printf 'window.customConfig.%s="%s";\n' "$name" "$value"
+        printf 'window.customConfig.%s=`%s`;\n' "$name" "$value"
     fi
 done
